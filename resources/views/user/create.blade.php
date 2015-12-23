64f11e0aeaacc688fc22874d5c0254d6778e6635
Sheraz Sarwar <sheraz786@gmail.com>
2015-12-23 14:48:47 +0000

    Improvements to frontend views of user index and registration pages

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,27 +1,54 @@
 @extends('layouts.master')
 
 @section('content')
+    @if ($errors->any())
+        <ul class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
     {!! Form::open(['route' => 'user.store']) !!}
         <div class="form-group">
             {!! Form::label('first_name', 'First Name') !!}
-            {!! Form::text('first_name', null, ['class' => 'form-control']) !!}
+            {!! Form::text('first_name', null, ['class' => 'form-control', 'placeholder' => 'e.g. Mohammed']) !!}
         </div>
         <div class="form-group">
             {!! Form::label('last_name', 'Last Name') !!}
-            {!! Form::text('last_name', null, ['class' => 'form-control']) !!}
+            {!! Form::text('last_name', null, ['class' => 'form-control', 'placeholder' => 'e.g. Khan']) !!}
         </div>
         <div class="form-group">
             {!! Form::label('email', 'Email') !!}
-            {!! Form::text('email', null, ['class' => 'form-control']) !!}
+            {!! Form::text('email', null, ['class' => 'form-control', 'placeholder' => 'e.g. example@email.com']) !!}
         </div>
         <div class="form-group">
             {!! Form::label('gender', 'Gender') !!}
-            <label class="radio-inline">
-                {!! Form::radio('gender', 'male', ['class' => 'form-control']) !!} Male
-            </label>
-            <label class="radio-inline">
-                {!! Form::radio('gender', 'female', ['class' => 'form-control']) !!} Female
-            </label>
+            <div class="row col-md-offset-0">
+                <label class="radio-inline">
+                    {!! Form::radio('gender', 'male', ['class' => 'form-control']) !!} Male
+                </label>
+                <label class="radio-inline">
+                    {!! Form::radio('gender', 'female', ['class' => 'form-control']) !!} Female
+                </label>
+            </div>
+        </div>
+        <div class="form-group">
+            {!! Form::label('day', 'Date of Birth') !!}
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="row">
+                        <div class="col-xs-2">
+                            {!! Form::text('day', null, ['class' => 'form-control', 'placeholder' => 'DD']) !!}
+                        </div>
+                        <div class="col-xs-2">
+                            {!! Form::text('month', null, ['class' => 'form-control', 'placeholder' => 'MM']) !!}
+                        </div>
+                        <div class="col-xs-3">
+                            {!! Form::text('year', null, ['class' => 'form-control', 'placeholder' => 'YYYY']) !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="form-group">
             {!! Form::label('password', 'Password') !!}
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="row">
+        @if (isset($users) && !empty($users))
+            <table class="table table-hover">
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                </tr>
+                @foreach ($users as $user)
+                    <tr>
+                        <td>{{ $user->first_name }}</td>
+                        <td>{{ $user->last_name }}</td>
+                        <td>{{ $user->email }}</td>
+                    </tr>
+                @endforeach
+            </table>
+        @endif
+        {!! link_to_route('user.create', '+ Create a new user', null, ['class' => 'btn btn-primary']) !!}
+    </div>
+@stop
