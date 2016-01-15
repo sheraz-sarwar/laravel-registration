bde78fa1dbea8228f92dc82bfcd41ff205055918
Sheraz Sarwar <sheraz@sheraz.me.uk>
2016-01-15 10:49:01 +0000

    Some configuration for sending emails

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => env('MAIL_FROM', 'admin@digitalocean.com'), 'name' => env('MAIL_NAME', 'Admin')],
 
     /*
     |--------------------------------------------------------------------------
