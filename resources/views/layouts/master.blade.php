8d73098e4b01eae6d3cb9a5ac812ad22cfcdd81f
Sheraz Sarwar <sheraz@sheraz.me.uk>
2016-01-15 10:58:58 +0000

    Some inline CSS, to be moved to external stylesheet

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,7 +2,26 @@
 <html>
 <head>
     <title>Registration</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/paper/bootstrap.min.css" rel="stylesheet">
+    <style>
+    body {
+        font-family: 'Lato';
+        padding-top: 50px;
+        background-color: #eaeaea;
+    }
+
+    label {
+        font-size: 19px;
+        color: #404040;
+    }
+    .form-container, .user-list {
+        background-color: #fff;
+        padding: 20px;
+        margin-bottom: 50px;
+        box-shadow: rgba(0, 0, 0, 0.3) 15px 15px 20px;
+    }
+    </style>
 </head>
 <body>
     <div class="container">
